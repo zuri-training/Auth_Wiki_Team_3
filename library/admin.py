24eaace320a097610f1a3ab068fcd607537d6252
Kingsley Onyenze <105232000+KingPanda68@users.noreply.github.com>
2022-08-13 13:06:49 +0100

    Revert "Backend onimisea"

--- library/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AuthCode, Comment, Notification
-
-# Register your models here.
-admin.site.register(AuthCode)
-admin.site.register(Comment)
-admin.site.register(Notification)
